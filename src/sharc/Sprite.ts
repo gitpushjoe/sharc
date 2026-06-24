36b1ea70b980b32976e25e5545534a0bc4e8c8d6
gitpushjoe <josephobaraye@gmail.com>
2026-06-24 09:16:55 -0400

    Implement Property Visibility & Optional and Property.Calculated

--- src/sharc/Sprite.ts
+++ src/sharc/Sprite.ts
@@ -2,22 +2,19 @@ export type Stage = {
     markDirty: () => void;
 };
 
-export type Sprite = {
-    stage: Stage;
-};
-
 export type VisibilityEnum = "hidden" | "visible";
 
 export type PropertyTypeEnum = "normal" | "aggregate" | "calculated";
 
-export type Property<Type> = {
-    get: () => Type;
-    set: (value: Type) => void;
-    readonly _type?: Type; // not used at runtime; TODO: allow for undefined values(?)
+export type Property<Type, Visibility extends boolean, Optional extends boolean> = {
+    get: (this: Sprite<Type>) => Type;
+    set: (this: Sprite<Type>, value: Type) => void;
+    readonly _type?: Type; // not used at runtime
 } & {
     configurable: false;
-} & ThisType<Sprite> & {
-        visibility: VisibilityEnum;
+} & ThisType<Sprite<Type>> & {
+        visible: Visibility;
+        optional: Optional;
         propertyType: PropertyTypeEnum;
     };
 
@@ -34,11 +31,21 @@ export const test = <T extends string>(t: T): Record<T, number> => {
     return thing;
 };
 
+type Attributes<Visibility extends boolean = boolean, Optional extends boolean = boolean> = {
+    readonly visible?: Visibility;
+    readonly optional?: Optional;
+};
+
 export const Property = {
-    Normal: <Type, Name extends string>(name: Name, defaultValue: Type): { [K in Name]: Property<Type> } => {
+    Normal: <Type, Name extends string, Visibility extends boolean = false, Optional extends boolean = false>(
+        name: Name,
+        defaultValue: Type,
+        attributes: Attributes<Visibility, Optional> = { visible: false as Visibility, optional: false as Optional }
+    ): { [K in Name]: Property<Type, Visibility, Optional> } => {
         const symbol = Symbol(name);
-        const prop: Property<Type> = {
-            visibility: "visible",
+        const prop: Property<Type, Visibility, Optional> = {
+            visible: attributes.visible ?? (false as Visibility),
+            optional: attributes.optional ?? (false as Optional),
             propertyType: "normal",
             configurable: false,
             get: function () {
@@ -46,7 +53,7 @@ export const Property = {
                 return this[symbol];
             },
             set: function (value) {
-                this[symbol] = value;
+                (this as Record<symbol, any>)[symbol] = value;
             }
         };
         return {
@@ -56,66 +63,171 @@ export const Property = {
                 writable: true,
                 configurable: false
             } as NormalPropertyImpl<Type> as any
-        } as { [K in Name]: Property<Type> };
+        } as { [K in Name]: Property<Type, Visibility, Optional> };
     },
-    Aggregate: <T extends any, Mapping extends Record<string, [T, string]>, Name extends string>(
+    Calculated: <
+        Type extends any,
+        Name extends string,
+        Visibility extends boolean = false,
+        Optional extends boolean = false
+    >(
         name: Name,
-        mappings: Mapping
-    ): { [K in Name]: Property<{ [L in keyof Mapping]: Mapping[L][0] }> } => {
+        methods: { get: Property<Type, Visibility, Optional>["get"]; set: Property<Type, Visibility, Optional>["set"] },
+        attributes: Attributes<Visibility, Optional> = { visible: false as Visibility, optional: false as Optional }
+    ): { [K in Name]: Property<Type, Visibility, Optional> } => {
+        const prop: Property<Type, Visibility, Optional> = {
+            visible: attributes.visible ?? (false as Visibility),
+            optional: attributes.optional ?? (false as Optional),
+            propertyType: "aggregate",
+            configurable: false,
+            get: methods.get,
+            set: methods.set
+        };
+        return {
+            [name]: prop
+        } as { [K in Name]: Property<Type, Visibility, Optional> };
+    },
+    Aggregate: <
+        T extends any,
+        Mapping extends Record<string, [T, string]>,
+        Name extends string,
+        Visibility extends boolean = false,
+        Optional extends boolean = false
+    >(
+        name: Name,
+        mappings: Mapping,
+        attributes: Attributes<Visibility, Optional> = { visible: false as Visibility, optional: false as Optional }
+    ): { [K in Name]: Property<{ [L in keyof Mapping]: Mapping[L][0] }, Visibility, Optional> } => {
         const fakeNames: (keyof Mapping)[] = [];
         const realNames: Mapping[string][1][] = [];
         for (const [fake, real] of Object.entries(mappings)) {
             fakeNames.push(fake);
             realNames.push(real[1]);
         }
-        const prop: Property<{ [K in keyof Mapping]: Mapping[K][0] }> = {
-            visibility: "visible",
-            propertyType: "aggregate",
-            configurable: false,
-            get: function () {
-                const res: { [K in keyof Mapping]: Mapping[K][0] } = {} as any;
-                for (let i = 0; i < fakeNames.length; ++i) {
-                    res[fakeNames[i] as keyof Mapping] = (this as Record<Mapping[string][1], T>)[realNames[i]];
-                }
-                return res;
-            },
-            set: function (value) {
-                for (let i = 0; i < fakeNames.length; ++i) {
-                    const val = value[fakeNames[i]];
-                    if (!val) {
-                        continue;
+        return Property.Calculated(
+            name,
+            {
+                get: function () {
+                    const res: { [K in keyof Mapping]: Mapping[K][0] } = {} as any;
+                    for (let i = 0; i < fakeNames.length; ++i) {
+                        res[fakeNames[i] as keyof Mapping] = (this as Record<Mapping[string][1], T>)[realNames[i]];
                     }
-                    (this as Record<Mapping[string][1], T>)[realNames[i]] = val;
+                    return res;
+                },
+                set: function (value) {
+                    for (let i = 0; i < fakeNames.length; ++i) {
+                        const val = value[fakeNames[i]];
+                        if (!val) {
+                            continue;
+                        }
+                        (this as Record<Mapping[string][1], T>)[realNames[i]] = val;
+                    }
                 }
-            }
-        };
-        return {
-            [name]: prop
-        } as { [K in Name]: Property<{ [L in keyof Mapping]: Mapping[L][0] }> };
+            },
+            attributes
+        );
     }
 };
 
-const prop = {
-    ...Property.Normal("x", 1024),
-    ...Property.Normal("y", 1024),
-    ...Property.Normal("z", 1024),
-    ...Property.Aggregate("pos", { x: [0, "x"], y: [0, "y"], z: [0, "z"] }),
-    ...Property.Aggregate("pos2", { x2: [0, "x"], y2: [0, "y"], z2: [0, "z"] })
+type SpriteBase = typeof SpriteBase;
+
+type Blueprint<Base extends SpriteBase, Properties extends Record<string, Property<any, boolean, boolean>>> = {
+    base: Base;
+    properties: Properties;
 };
 
-const makeSprite = <Props extends Record<string, Property<any>>>(
-    props: Props
-): {
-    [K in keyof typeof props]: Required<Props[K]>["_type"];
-} => {
-    const obj = {};
-    Object.defineProperties(obj, props);
-    return obj as any;
+const Blueprint = <Base extends SpriteBase, Properties extends Record<string, Property<any, boolean, boolean>>>(
+    base: Base,
+    properties: Properties
+) => {
+    return Object.freeze({ base, properties });
 };
 
-const obj = makeSprite(prop);
-// obj.x = 42;
-//
-obj.pos2 = { x2: 10, y2: 10, z2: -6 };
+/** todo: come up with an actually good name for this */
+// a disgusting abomination:
+type Rendered<
+    Properties extends Record<string, Property<any, boolean, boolean>>,
+    Requirements extends { visible: boolean } = { visible: boolean }
+> = Pick<
+    {
+        //                     👇 note the question mark here
+        [K in keyof Properties]?: Required<Properties[K]>["_type"];
+    },
+    keyof {
+        [K in keyof Properties as Properties[K]["visible"] extends Requirements["visible"]
+            ? Properties[K]["optional"] extends true
+                ? K
+                : never
+            : never]: any;
+    }
+> &
+    Pick<
+        {
+            [K in keyof Properties]: Required<Properties[K]>["_type"];
+        },
+        keyof {
+            [K in keyof Properties as Properties[K]["visible"] extends Requirements["visible"]
+                ? Properties[K]["optional"] extends true
+                    ? never
+                    : K
+                : never]: any;
+        }
+    >;
 
-console.log(obj.pos, obj.x, obj.y, obj.z, obj.pos2);
+type Sprite<Properties = Record<string, any>> = SpriteBase & Properties & Record<symbol, any>;
+
+const makeSpriteClass = <Base extends SpriteBase, Properties extends Record<string, Property<any, boolean, boolean>>>(
+    blueprint: Blueprint<Base, Properties>
+) => {
+    const proto = { ...blueprint.base };
+    Object.defineProperties(proto, blueprint.properties);
+
+    return (props: Partial<Rendered<Properties, { visible: true }>> = {}): Sprite<Rendered<Properties>> => {
+        const sprite = {};
+        Object.setPrototypeOf(sprite, proto);
+        const entries = Object.entries(props);
+        for (let i = 0; i < entries.length; ++i) {
+            const [lhs, rhs] = entries[i];
+            sprite[lhs] = rhs;
+        }
+        return sprite as any;
+    };
+};
+
+// ...
+
+const SpriteBase = Object.freeze({
+    stage: undefined as Stage | undefined
+});
+
+const SpriteBlueprint = Blueprint(
+    { ...SpriteBase },
+    {
+        ...Property.Aggregate("bounds", { x1: [0, "x1"], y1: [0, "y1"], x2: [0, "x2"], y2: [0, "y2"] }, {
+            visible: true
+        } as const),
+        ...Property.Normal("x1", 0, { visible: false } as const),
+        ...Property.Normal("y1", 0, { visible: false } as const),
+        ...Property.Normal("x2", 0, { visible: false } as const),
+        ...Property.Normal("y2", 0, { visible: false } as const),
+        ...Property.Normal("enabled", true, { visible: true } as const)
+    }
+);
+
+const Sprite = makeSpriteClass(SpriteBlueprint);
+
+// ...
+
+const EllipseBlueprint = Blueprint(
+    { ...SpriteBlueprint.base },
+    { ...SpriteBlueprint.properties, ...Property.Normal("radius", 10, { visible: true } as const) }
+);
+
+const Ellipse = makeSpriteClass(EllipseBlueprint);
+type Ellipse = ReturnType<typeof Ellipse>;
+
+// ...
+
+const ellipse1 = Ellipse({ enabled: true, radius: 5 });
+const ellipse2 = Ellipse({ enabled: true, bounds: { x1: 10, y1: 10, x2: 20, y2: 20 } });
+console.log(ellipse1, ellipse2);
